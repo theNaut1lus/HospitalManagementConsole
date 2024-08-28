4f5ec8c6d81ba0884909494d24c1edddfa9f09a0
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-08-28 11:38:43 +1000

    Initial project setup, class files and userdata static files

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementConsole
+{
+    public abstract class User
+    {
+        public string id, password, fullame, role;
+        public string[] options = Array.Empty<string>();
+
+        public User(string id, string password, string fullname, string role)
+        {
+            this.id = id;
+            this.password = password;
+            this.fullame = fullname;
+            this.role = role;
+        }
+
+    }
+}
